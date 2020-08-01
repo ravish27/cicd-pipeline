6619ee381223f7ec572091abec3e1a360e858716
Ravish Sehgal <er.ravishsehgal@gmail.com>
2020-08-01 15:14:26 +0530

    added vars

--- ansible/part1_create_vm.sh
+++ ansible/part1_create_vm.sh
@@ -10,9 +10,10 @@
 readonly PROJECT='devops-training-281306'
 readonly SERVICE_ACCOUNT='demo-gcp-gce-ansible@devops-training-281306.iam.gserviceaccount.com'
 readonly ZONE='us-central1-a'
+readonly GCP_JSON='/home/rsehgal/devops-training-281306-a5aae0650847.json'
 
-time gcloud config set account demo-gcp-gce-ansible@devops-training-281306.iam.gserviceaccount.com
-time gcloud auth activate-service-account demo-gcp-gce-ansible@devops-training-281306.iam.gserviceaccount.com --key-file /home/rsehgal/devops-training-281306-a5aae0650847.json
+time gcloud config set account $SERVICE_ACCOUNT
+time gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file $GCP_JSON
 
 time gcloud compute instances create web-ravish27-devops \
 --project=$PROJECT \
